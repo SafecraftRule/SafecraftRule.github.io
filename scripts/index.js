f6585c4ede2d28f8bb28faa697fc90875549dcc1
SafecraftRule <dawoemail1200@gmail.com>
2025-01-18 11:44:38 +0100

    Main Update

--- scripts/index.js
+++ scripts/index.js
@@ -2,20 +2,27 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.0.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/9.0.0/firebase-auth.js";
 
+//Die firebase variablen
+let config;
+let app;
+let auth;
+
 fetch("../config/config.json")
   .then((response) => response.json()) // Antwort als JSON parsen
   .then((firebaseConfig) => {
-    // Firebase initialisieren
+    //Firebase konfigurationen
+    config = firebaseConfig;
+    app = initializeApp(config);
+    auth = getAuth(app);
+
     if (!firebaseConfig) {
       throw new Error("Firebase-Konfiguration nicht gefunden");
     }
-
-    // Firebase mit der geladenen Konfiguration initialisieren
-    const app = initializeApp(firebaseConfig);
-
-    const auth = getAuth(app);
-    console.log("Firebase erfolgreich initialisiert");
+    console.log("Firebase erfolgreich initialisiert!");
   })
   .catch((error) => {
-    console.error("Fehler beim Laden oder Initialisieren von Firebase:", error);
+    console.error(
+      "Fehler beim Laden oder Initialisieren von Firebase: ",
+      error
+    );
   });
