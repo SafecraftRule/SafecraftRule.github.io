e3b38481a31d7157555bf8c2805b7e21c7f65259
SafecraftRule <dawoemail1200@gmail.com>
2025-03-26 13:13:31 +0100

    Main Update

--- scripts/main.js
+++
@@ -1,6 +0,0 @@
-const openbtn = document.getElementById("connectdiabtn");
-
-openbtn.addEventListener("click", function () {
-  //Dialog zeigen
-  document.getElementById("connectdialog").showModal();
-});
